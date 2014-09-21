5c066752cbe4e329eac1216ccfc4ae6ffacce40c
冯文春 <707908885@qq.com>
2014-09-21 16:57:34 +0800

    添加set和get方法

--- ccmm/src/com/tdrz/util/DBUtils.java
+++ ccmm/src/com/tdrz/util/DBUtils.java
@@ -7,5 +7,32 @@ public class DBUtils {
 	private String db;
 	
    private String username;
+
+public String getConn() {
+	return conn;
+}
+
+public void setConn(String conn) {
+	this.conn = conn;
+}
+
+public String getDb() {
+	return db;
+}
+
+public void setDb(String db) {
+	this.db = db;
+}
+
+public String getUsername() {
+	return username;
+}
+
+public void setUsername(String username) {
+	this.username = username;
+}
 	
+
+
+
 }
